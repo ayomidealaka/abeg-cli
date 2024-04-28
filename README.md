8cc3af3c36876b0b1f59d8a42debe4e14a523290
Ayomide Alaka-Yusuf <muizzyomide@gmail.com>
2024-04-28 10:28:24 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # abeg-cli
+
+abeg-cli is a Command Line Interface (CLI) companion designed to streamline your CLI workflow by generating quick command line instructions for common tasks. Acting like a co-pilot for your terminal, abeg-cli translates natural language descriptions into succinct, executable commands.
+
+This currently uses OpenAI's gpt-4-0125-preview model.
+
+## How it Works
+
+Simply tell abeg-cli what you need in plain English, and it will provide you with the exact command to execute. Whether you need to stop all services running on a particular port, set up a local server, or manage containerized applications, abeg-cli is your go-to assistant for fast and reliable command generation.
+
+## Example Usage
+
+Imagine you need to stop all running services on port 3000. Instead of scouring the web for solutions, simply type:
+
+```bash
+abeg command to stop all running services on port 3000
+```
+
+'abeg-cli' will immediately respond with a command like:
+
+```bash
+kill $(lsof -t -i:3000)
+```
+
+Run the returned command in your terminal, and your task is done!
+
+## Configuration
+
+Before using the CLI tool, you need to set up your OpenAI API key as an environment variable. This ensures secure authentication with the OpenAI service.
+
+### Obtaining Your OpenAI API Key
+
+If you don't already have an OpenAI API key, you can obtain one by signing up or logging into your account at OpenAI API.
+
+### Setting Up Your API Key
+
+Set the required environment variables:
+
+```bash
+export OPENAI_API_KEY='your_api_key_here'
+```
---
+++ abeg.py
@@ -0,0 +1,87 @@
+# from yaspin import yaspin
+import argparse
+import sys
+import platform
+import os
+from openai import OpenAI
+
+class Style:
+    GREY = '\033[90m'
+    RED = '\033[31m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    DIM = '\033[2m'
+    CYAN = '\033[36m'
+    UNDERLINE = '\033[4m'
+    YELLOW = "\033[1;33m"
+    GREEN = "\033[0;32m"
+
+# Attempt to get the API key from an environment variable
+api_key = os.getenv('OPENAI_API_KEY')
+
+if api_key is None:
+    print(f"{Style.RED}Error:{Style.RESET} OpenAI API key not found. Please set the OPENAI_API_KEY environment variable.")
+    exit(1)
+
+client = OpenAI(
+    api_key=api_key
+)
+
+def print_custom_help():
+    custom_help_text = f"""
+ Usage: {Style.BOLD}abeg{Style.RESET} [command]
+"""
+    print(custom_help_text)
+
+def requestFromAI(question):
+    architecture = platform.machine()
+    os_name = os.name
+    sysinfo = f"Architecture: {architecture}, OS: {os_name}"
+    prompt = f"""
+        You are ABEG, a CLI code generator. Respond with the CLI command to generate the code with only one short sentence description in first line 
+        Your response should always and MUST always be accompanied with only one short sentence description in the first line even when the user repeats the same question.
+		If the user asks for a specific language, respond with the CLI command to generate the code in that language.
+		If CLI command is multiple lines, separate each line with a newline character and always have the one short sentence description in the first line before the command.
+		Do not write any markdown. Do not write any code.
+        System Info: {sysinfo}
+    
+		First line is the description in one sentence.
+		
+        Example output:
+
+		Building and installing a Go binary
+		go build main.go
+		go install main
+        """
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4-0125-preview",
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": question}
+            ]            
+        )
+        response_content = response.choices[0].message.content
+        lines = response_content.split('\n')
+
+        formatted_lines = [f"{Style.YELLOW}{lines[0]}{Style.RESET}"] + \
+                        [f"$ {line}" for line in lines[1:]]
+
+        formatted_output = '\n'.join(formatted_lines)
+        print(formatted_output)
+
+        return response.choices[0].message.content
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def main():
+    if "-h" in sys.argv or "--help" in sys.argv:
+        print_custom_help()
+    else:
+        command_description = ' '.join(sys.argv[1:])
+        requestFromAI(command_description)
+        
+if __name__ == "__main__":
+    main()
